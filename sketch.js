3e4f5094b703cba29a3c8dd09e31918244c86ed6
Shiislaja <janko.cholvadt@gmail.com>
2025-03-10 09:43:40 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 200);
+  noLoop(); // Vykreslíme len raz
+}
+
+function draw() {
+  background(220);
+
+  let xoff = 0; // Počiatočný offset pre Perlin noise
+
+  for (let x = 0; x < width; x++) {
+    let gray = noise(xoff); // Získaj hodnotu noise pre aktuálny offset
+    stroke(gray * 255); // Nastav odtieň šedej
+    line(x, height / 2 - 10, x, height / 2 + 10); // Nakresli krátku čiaru
+    xoff += 0.005; // Posuň offset o malú hodnotu
+  }
+}
